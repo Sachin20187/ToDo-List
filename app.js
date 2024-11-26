2c428532cd1afed25dfad13196484d5951ae75c2
Sachin <sachinkumar.20187@gmail.com>
2024-11-26 20:00:33 +0530

    updates

--- app.js
+++ app.js
@@ -78,3 +78,57 @@ ul.addEventListener("click", function(event){
 })
 
 
+// function addTask() {
+//     const taskInput = document.getElementById('input');
+//     const taskList = document.getElementById('taskList');
+//     const taskText = taskInput.value.trim();
+
+//     if (taskText === '') return;
+
+//     const taskItem = document.createElement('li');
+//     taskItem.className = 'list';
+//     taskItem.innerHTML = `
+//         <input type="checkbox" onchange="updateTaskStatus(this)">
+//         <span class="task-text">${taskText}</span>
+//         <button class="delete", onclick="deleteTask(this)">Delete</button>
+//     `;
+
+//     taskList.appendChild(taskItem);
+//     taskInput.value = '';
+//     updateTaskCount();
+// }
+
+// taskInput.addEventListener("keypress", function(event) {
+//         if ((event.key === "Enter") && (taskText !== "")){
+//           event.preventDefault();
+//           document.getElementById("btn").click();
+//         }
+//     });
+
+// function deleteTask(button) {
+//     const taskItem = button.parentElement;
+//     taskItem.remove();
+//     updateTaskCount();
+// }
+
+// function updateTaskStatus(checkbox) {
+//     const taskItem = checkbox.parentElement;
+//     if (checkbox.checked) {
+//         taskItem.classList.add('completed');
+//     } else {
+//         taskItem.classList.remove('completed');
+//     }
+//     updateTaskCount();
+// }
+
+// function updateTaskCount() {
+//     const taskList = document.getElementById('taskList');
+//     const totalTasks = taskList.children.length;
+//     const completedTasks = taskList.querySelectorAll('.list.completed').length;
+
+//     document.getElementById('totalTasks').innerText = `Total task: ${totalTasks}`;
+//     document.getElementById('completedTasks').innerText = `Task completed: ${completedTasks}`;
+// }
+
+// // Initial count update
+// updateTaskCount();
